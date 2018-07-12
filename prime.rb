2e94431cd0481fe8417ae4b5aa16e7c0247f9b9c
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-12 11:41:14 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,18 @@
+# def prime?(num)
+#   if num < 0 or num == 0 or num == 1
+#     return false
+#   else
+#     (2..num-1).to_a.all? do |possible_factor|
+#       num % possible_factor != 0
+#     end
+#   end
+# end
 
 def prime?(n)
   if n < 0  || n == 0 || n == 1
     return false
-    
   else (2..num-1).to_a.all? { |x|  n % x != 0} 
-
+end
 end
 end
 
