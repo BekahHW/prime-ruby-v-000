13d4f0f872612cef9ed77f977338d284183a9570
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-12 11:10:05 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,37 @@
-# Add  code here!
-prime?(num)
-  if num == "2"
-    return TRUE
-  else
-    num.odd? &&  number % 3 == 0 ? false : true
+def prime?(n)
+  if n.odd? { |x| n % 3 == 0} 
+    return true
+  else num < 0 or num == 0 or num == 1
+    return false
 end
 end
 
+# def prime?(n)
+#   if n == 2 || n.odd? { |x| n % 3 != 0}
+#     true
+#   elsif num < 0 || num == 0 || num == 1
+#     return false
+#   else 
+#     false
+# end
+# end
+
+# def prime?(n)
+#   if n == "2"
+#     return true
+#   elsif n.odd? { |x| n % 3 != 0}
+#     return true
+#   # elsif n.all?(&:negative?) 
+#   #   return false
+#   else 
+#     return true
+# end
+# end
+# all_odd = [1,3].all? do |number|
+# number.odd?
+
+
+# Prime != 2 && num.odd? != num % 3
+
+# def divisible?(n)
+#   (1..20).all? { |x| n % x == 0 }
