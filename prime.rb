fe3bb5f45ba9aa7952b7f02427ab0e68cb0ffc5d
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-12 11:29:26 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -5,7 +5,7 @@ def prime?(n)
   elsif n < 0  || n == 0 || n == 1
     return false
     
-  elsif n.odd? { |x| (n % x).zero?} 
+  elsif n.odd? { |x| return false if n % x == 0} 
     return true
 
  
