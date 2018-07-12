f8e7b6e2fb351d44dccfdd63122f9a09b634d585
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-12 11:14:58 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,12 +2,13 @@ def prime?(n)
   if n == 2 || n == 3
     return true
     
+  elsif n < 0  || n == 0 || n == 1
+    return false
+    
   elsif n.odd? { |x| n % 3 != 0} 
     return true
 
  
-  elsif n < 0  || n == 0 || n == 1
-    return false
   else
     return false
 end
