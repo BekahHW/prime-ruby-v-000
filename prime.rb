994b9e78f364bf5ba75c1b5cbcefdec8e7c5baf0
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-12 11:35:46 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -12,35 +12,35 @@ def prime?(n)
   primes
 end
 
-def get_prime_no_upto(number)
-  start = 2
-  primes = (start..number).to_a
-  (start..number).each do |no|
-    (start..no).each do |num|
-      if ( no % num  == 0) && num != no
-        primes.delete(no)
-        break
-      end
-    end
-  end
-  primes
-end
+# def get_prime_no_upto(number)
+#   start = 2
+#   primes = (start..number).to_a
+#   (start..number).each do |no|
+#     (start..no).each do |num|
+#       if ( no % num  == 0) && num != no
+#         primes.delete(no)
+#         break
+#       end
+#     end
+#   end
+#   primes
+# end
 
-def prime?(n)
-  if n == 2 || n == 3
-    return true
+# def prime?(n)
+#   if n == 2 || n == 3
+#     return true
     
-  elsif n < 0  || n == 0 || n == 1
-    return false
+#   elsif n < 0  || n == 0 || n == 1
+#     return false
     
-  elsif n.odd? { |x|  n % x != 0} 
-    return true
+#   elsif n.odd? { |x|  n % x != 0} 
+#     return true
 
  
-  else
-    return false
-end
-end
+#   else
+#     return false
+# end
+# end
 
 # def prime?(n)
 #   if n == 2 || n.odd? { |x| n % 3 != 0}
